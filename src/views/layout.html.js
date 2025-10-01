88eb5e71c05d61166736d147d320cf437f69f403
artemave <artemave@gmail.com>
2025-10-01 20:27:31 +0200

    Fix white off-screen background on ios

--- src/views/layout.html.js
+++ src/views/layout.html.js
@@ -15,7 +15,7 @@ export default function layout({
     <html lang="en">
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         <title>${title}</title>
         <link rel="stylesheet" href="/styles.css" />
         <meta name="theme-color" content="#0f172a" />
